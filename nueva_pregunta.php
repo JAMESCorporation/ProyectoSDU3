09ef35ba5a2aa08f53e5bc6bb3ae0b0fc01f0036
joker <joker@joker-PC>
2016-05-31 17:51:28 -0500

    pregunta

---
+++ nueva_pregunta.php
@@ -0,0 +1,51 @@
+<?php 
+require_once("includes/conexion.php");	
+$pregunta = $_POST['pregunta'];
+$res1 = $_POST['res1'];
+$res2 = $_POST['res2'];
+$res3 = $_POST['res3'];
+$correcta = $_POST['correcta'];
+
+$id_test = 2;
+
+$sql = "INSERT INTO Pregunta VALUES (null,'$pregunta',$id_test)";
+$res = mysqli_query($con, $sql) or die (myqli_connect_error());
+
+$sql = "select id_pregunta from Pregunta order by id_pregunta desc limit 1";
+$res = mysqli_query($con, $sql) or die (myqli_connect_error());
+$reg = mysqli_fetch_array($res) or die (myqli_connect_error());
+
+$id_pregunta = $reg['0'];
+
+	if($correcta == 1){
+		$sql = "INSERT INTO Respuesta VALUES (null,'$res1',1,$id_pregunta)";
+		$res = mysqli_query($con, $sql) or die (myqli_connect_error());
+
+		$sql = "INSERT INTO Respuesta VALUES (null,'$res2',0,$id_pregunta)";
+		$res = mysqli_query($con, $sql) or die (myqli_connect_error());
+
+		$sql = "INSERT INTO Respuesta VALUES (null,'$res3',0,$id_pregunta)";
+		$res = mysqli_query($con, $sql) or die (myqli_connect_error());
+	}
+	if($correcta == 2){
+		$sql = "INSERT INTO Respuesta VALUES (null,'$res1',0,$id_pregunta)";
+		$res = mysqli_query($con, $sql) or die (myqli_connect_error());
+
+		$sql = "INSERT INTO Respuesta VALUES (null,'$res2',1,$id_pregunta)";
+		$res = mysqli_query($con, $sql) or die (myqli_connect_error());
+
+		$sql = "INSERT INTO Respuesta VALUES (null,'$res3',0,$id_pregunta)";
+		$res = mysqli_query($con, $sql) or die (myqli_connect_error());
+	}
+	if($correcta == 3){
+		$sql = "INSERT INTO Respuesta VALUES (null,'$res1',0,$id_pregunta)";
+		$res = mysqli_query($con, $sql) or die (myqli_connect_error());
+
+		$sql = "INSERT INTO Respuesta VALUES (null,'$res2',0,$id_pregunta)";
+		$res = mysqli_query($con, $sql) or die (myqli_connect_error());
+
+		$sql = "INSERT INTO Respuesta VALUES (null,'$res3',1,$id_pregunta)";
+		$res = mysqli_query($con, $sql) or die (myqli_connect_error());
+	}
+
+?>
